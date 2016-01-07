191cb9be8bba8e62ae16854b29e183b552bfb510
Ehsan Rafeeque <dreamer_2142@yahoo.com>
2016-01-07 18:22:13 +0000

    Article route for getById and getByCategory

--- routes/articles.js
+++ routes/articles.js
@@ -11,7 +11,31 @@ router.get('/', function(req, res, next) {
             console.log(err);
         }
         res.json(data);
-    })    
-});
+    });
+    
+})
+.get('/:id',function(req,res,next) {
+    console.log("NEXT");
+    var id = req.params.id;
+    // res.send("ID : "+id);
+    // console.log(id);
+    Article.getArticleById(id,function(err,data){
+        if(err){
+            console.log(err);
+        }
+        res.json(data);
+    });
+})
+.get('/category/:category',function(req,res,next){
+    var category = req.params.category;
+    Article.getArticleByCategory(category,function(err,data){
+        if(err){
+            console.log(err);
+        }
+        res.json(data);
+    })
+})
+
+
 
 module.exports = router;
